cd10ef57e09a4fdbf0cf00c7ee3218055e5c15db
Nazar Kalinchuk <nazariy2410@gmail.com>
2018-09-16 17:51:13 +0300

    dev

---
+++ src/js/search.js
@@ -0,0 +1,24 @@
+let search = {
+
+    init: function () {
+        this.events();
+    },
+
+
+
+    events: function () {
+        let self = this;
+        $('.selectpicker').selectpicker();
+
+
+
+
+
+    }
+};
+
+
+
+jQuery(function () {
+    search.init();
+});
