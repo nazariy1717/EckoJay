d045a4901ffbcf2f6d81465f4bf79c07bac03a54
Nazar Kalinchuk <nazariy2410@gmail.com>
2018-09-16 19:14:36 +0300

    dev

--- src/js/search.js
+++ src/js/search.js
@@ -8,9 +8,21 @@ let search = {
 
     events: function () {
         let self = this;
-        $('.selectpicker').selectpicker();
 
 
+        $( function() {
+            var handle = $( "#custom-handle" );
+            $( "#slider" ).slider({
+                min: 0,
+                max: 1000000,
+                create: function() {
+                    handle.text( $( this ).slider( "value" ) );
+                },
+                slide: function( event, ui ) {
+                    handle.text( ui.value );
+                }
+            });
+        } );
 
 
 
